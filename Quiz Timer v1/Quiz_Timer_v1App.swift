6760f59fd9b50fb66c9382aeefa3ca5aba66440c
Sanparty <88841545+Sanparty@users.noreply.github.com>
2023-02-10 17:06:23 -0500

    Initial Commit

---
+++ Quiz Timer v1/Quiz_Timer_v1App.swift
@@ -0,0 +1,17 @@
+//
+//  Quiz_Timer_v1App.swift
+//  Quiz Timer v1
+//
+//  Created by Dan San Pedro on 2023-02-10.
+//
+
+import SwiftUI
+
+@main
+struct Quiz_Timer_v1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
